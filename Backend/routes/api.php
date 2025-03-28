58ff2c126ea00ca703c69be8a338ba02ae318eb4
Kevin <kevindboy96@gmail.com>
2025-03-28 11:11:43 -0400

    backend costos

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\CostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/costs', [CostController::class, 'index']);
+Route::post('/costs', [CostController::class, 'store']);
+Route::put('/costs/{cost}', [CostController::class, 'update']);
+Route::delete('/costs/{cost}', [CostController::class, 'destroy']);
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
