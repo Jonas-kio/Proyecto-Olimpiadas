fd426a28f387880d5e7006845a607c45361519e1
Heynar Mamani <heynar.mamani.2000@gmail.com>
2025-04-08 09:38:13 -0400

    inicio anterior

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,45 +6,20 @@ import "./App.css";
 
 const App = () => {
   const [modoSeleccionado, setModoSeleccionado] = useState(null);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [loginExitoso, setLoginExitoso] = useState(false);
 
-  const handleLogin = () => {
-    // Login improvisado
-    if (email === "usuario@gmail.com" && password === "usuario123") {
-      setLoginExitoso(true);
-      setModoSeleccionado("usuario");
-    } else {
-      alert("Credenciales incorrectas.");
-    }
-  };
-
-  if (modoSeleccionado === "usuario" && loginExitoso) return <AppUsuario />;
+  if (modoSeleccionado === "usuario") return <AppUsuario />;
   if (modoSeleccionado === "admin") return <AppAdmin />;
 
+  // Pantalla de selección
   return (
     <div className="selector-container">
       <h1 className="titulo">Bienvenido</h1>
-      <p>email "usuario@gmail.com" password "usuario123"</p>
-      <input
-        type="email"
-        placeholder="Correo"
-        className="login-input"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Contraseña"
-        className="login-input"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button className="selector-btn" onClick={handleLogin}>
-        Iniciar Sesión
+      <button
+        className="selector-btn"
+        onClick={() => setModoSeleccionado("usuario")}
+      >
+        Usuarios
       </button>
-
       <button
         className="selector-btn"
         onClick={() => setModoSeleccionado("admin")}
