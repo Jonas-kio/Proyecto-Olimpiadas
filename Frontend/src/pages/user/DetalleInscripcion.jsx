73e4409d49926990f1214a7162aef89af8e5381b
JuanCarlosLlanosVega <juancarlosllanosvega123@gmail.com>
2025-04-14 17:11:36 -0400

    Restructuración del botón de Detalle de Inscripcion y sus dimenciones

--- Frontend/src/pages/user/DetalleInscripcion.jsx
+++ Frontend/src/pages/user/DetalleInscripcion.jsx
@@ -4,6 +4,8 @@ import "../../styles/components/Inscripcion.css";
 import ProcesandoModal from "../../components/common/ProcesandoModal";
 import ValidacionExitosaModal from "../../components/common/ValidacionExitosaModal";
 import { FaDownload, FaPrint } from "react-icons/fa";
+import { FaArrowLeft } from "react-icons/fa";
+
 
 const DetalleInscripcion = () => {
   const { id } = useParams();
@@ -101,13 +103,14 @@ const DetalleInscripcion = () => {
       <div className="detalle-contenedor"> 
 
       <div style={{ display: "flex", alignItems: "center", marginBottom: "20px" }}>
+
       <button
   onClick={() => window.history.back()}
   style={{
     backgroundColor: "#1d4ed8",
     border: "none",
-    width: "40px",
-    height: "40px",
+    width: "50px",
+    height: "50px",
     borderRadius: "8px",
     display: "flex",
     alignItems: "center",
@@ -117,21 +120,11 @@ const DetalleInscripcion = () => {
     marginRight: "12px"
   }}
 >
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    width="24"
-    height="24"
-    fill="none"
-    stroke="#ffffff"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-  >
-    <line x1="19" y1="12" x2="5" y2="12" />
-    <polyline points="12 19 5 12 12 5" />
-  </svg>
+  <FaArrowLeft color="white" size={24} />
 </button>
+
+
+
 
 
   <h1 style={{ fontSize: "20px", fontWeight: "600", color: "#1e3a8a", margin: 0 }}>
