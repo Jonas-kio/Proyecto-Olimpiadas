34d1935714938d30867b60de398e5f7fa0b76e3a
Isma9000 <145948205+Isma9000@users.noreply.github.com>
2025-04-26 07:32:26 -0400

    FEAT: integracion con el frontend y backend parte 2

--- Frontend/src/routes/AppRouterAdmin.jsx
+++ Frontend/src/routes/AppRouterAdmin.jsx
@@ -6,7 +6,7 @@ import DashboardConfig from "../pages/admin/config/DashboardConfig";
 import Reportes from "../pages/admin/config/Reportes";
 import HomeAdmin from "../pages/admin/HomeAdmin";
 import DasboardOlimpiada from '../pages/admin/DasboardOlimpiada';
-import FormularioOlimpiada from '../pages/admin/FormularioOlimpiada';
+import FormularioOlimpiada from "../pages/admin/formularioOlimpiada";
 
 const AppRouterAdmin = () => {
   return (
