aee3714a5f8a308aa8ec6a531e6809dcbb459f19
Isma9000 <145948205+Isma9000@users.noreply.github.com>
2025-05-26 19:24:20 -0400

    FEAT: Frontend de las boletas

--- Frontend/src/routes/AppRouterAdmin.jsx
+++ Frontend/src/routes/AppRouterAdmin.jsx
@@ -4,6 +4,7 @@ import { Routes, Route } from "react-router-dom";
 //import AreasConfig from "../pages/config/AreasConfig";
 import DashboardConfig from "../pages/admin/config/DashboardConfig";
 import Reportes from "../pages/admin/reportes/ReportPanel";
+import Boletas from "../pages/admin/boletas/Tickets";
 import HomeAdmin from "../pages/admin/HomeAdmin";
 import DasboardOlimpiada from '../pages/admin/DasboardOlimpiada';
 import FormularioOlimpiada from "../pages/admin/formularioOlimpiada";
@@ -18,6 +19,7 @@ const AppRouterAdmin = () => {
       <Route path="/formulario_Olimpiada/:id" element={<FormularioOlimpiada />} />
       <Route path="/configuracion" element={<DashboardConfig />} />
       <Route path="/reportes" element={<Reportes/>} />
+      <Route path="/boletas" element={<Boletas/>} />
     </Routes>
   );
 };
