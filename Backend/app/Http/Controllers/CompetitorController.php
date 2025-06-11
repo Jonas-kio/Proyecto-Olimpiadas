2d4976a509e7a757964c832774aab113eaa287a8
Kevin <kevindboy96@gmail.com>
2025-06-10 21:21:31 -0400

    competidor terminado

--- Backend/app/Http/Controllers/CompetitorController.php
+++ Backend/app/Http/Controllers/CompetitorController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Competitor;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
+
 
 class CompetitorController extends Controller
 {
